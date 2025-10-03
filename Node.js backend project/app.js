9ec1b4370d135b52dd753fc503cff83e0011283e
Oreoluwa <oreoluwa123503@gmail.com>
2025-10-03 15:03:32 +0100

    Moniepoint business integration and wallet funding

--- Node.js backend project/app.js
+++ Node.js backend project/app.js
@@ -7,6 +7,6 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.use('/api/deposit', require('./routes/deposit'));
+app.use('/api/deposit', require('../routes/deposit'));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
